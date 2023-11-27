0f564e3ce8b3d768b10bcbbe08fe2f38660b16d5
moonju <83450907+kimmoonju-102@users.noreply.github.com>
2023-11-27 23:30:38 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 9324 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+console.log(`${"long ".repeat(+input / 4)}int`);
