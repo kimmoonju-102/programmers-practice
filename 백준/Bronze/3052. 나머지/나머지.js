fad6d1bfa391ad62d4e189c66ff548b8f7cd9c9a
moonju <83450907+kimmoonju-102@users.noreply.github.com>
2023-12-07 14:41:28 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,20 +1,5 @@
-const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './example.txt';
-let input = fs.readFileSync(filePath).toString().split('\n');
-
-const items = input.map((item) => +item);
-solution(items);
-function solution(items) {
-    const answer = Array(42).fill(0);
-    for (let i = 0; i < 10; ++i) {
-        const nowChar = +items[i] % 42;
-        answer[nowChar]++;
-    }
-    let cnt = 0;
-    for (let i = 0; i < answer.length; ++i) {
-        if (answer[i] !== 0) {
-            cnt++;
-        }
-    }
-    console.log(cnt);
-}
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+	
+const count = new Set(input.map(x => x % 42)).size;
+    
+console.log(count);
