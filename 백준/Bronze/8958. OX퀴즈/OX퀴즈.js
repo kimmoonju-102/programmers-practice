64ed9fedc347dd6a3b6afa83ba83870f49508777
moonju <83450907+kimmoonju-102@users.noreply.github.com>
2023-12-08 16:49:17 +0900

    [Bronze II] Title: OX퀴즈, Time: 140 ms, Memory: 9568 KB -BaekjoonHub

---
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const num = +input[0];
+
+for (let i = 1; i <= num; i++) {
+  let count = 0;
+  let sum = 0;
+
+  for (let j = 0; j < input[i].length; j++) {
+    if (input[i][j] === "O") {
+      count++;
+    } else {
+      count = 0;
+    }
+    sum += count;
+  }
+  console.log(sum);
+}
