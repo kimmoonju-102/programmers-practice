39510168e76f642af2157755cfe323879197d7df
moonju <83450907+kimmoonju-102@users.noreply.github.com>
2024-04-17 23:13:23 +0900

    [Silver IV] Title: 스택, Time: 196 ms, Memory: 12408 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.js
+++ 백준/Silver/10828. 스택/스택.js
@@ -2,7 +2,6 @@ const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./example.txt";
 let input = fs.readFileSync(filePath).toString().trim().split("\n");
 
-const newInput = input.map((item) => item.split(" "));
 const len = input.shift();
 const stack = [];
 const result = [];
